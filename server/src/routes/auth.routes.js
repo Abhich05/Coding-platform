b62ae52a93d87ba87812c0f47a3a7859b58d725e
abhishek <abhihavinal805@gmail.com>
2026-03-23 00:30:00 +0000

    sudeep-server_structure

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 const express= require('express');
 const router=express.Router();
-const {registerUser}=require('../controllers/auth.controller');
+const {registerUser}=require('../../controllers/auth.controller');
 router.post('/register',registerUser);
 module.exports=router;
